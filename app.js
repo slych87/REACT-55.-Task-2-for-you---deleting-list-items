06cde7a71ebd3026b246bbee9ca88b8fe1721b56
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-02 10:00:58 +0200

    second solution

--- app.js
+++ app.js
@@ -18,18 +18,36 @@ class List extends React.Component {
       { id: 40, name: 'John S.' },
     ]
   }
-  handelDelete = (id) => {
-    const people = [...this.state.people]
-    const index = people.findIndex(person => person.id === id)
-    people.splice(index, 1);
+  // handelDelete = (id) => {
+  // const people = [...this.state.people]
+  //   const index = people.findIndex(person => person.id === id)
+  //   people.splice(index, 1);
+  //   this.setState({
+  //     people
+  //   })
+  // }
+
+  handelDelete(name) {
+    // let people = Array.from(this.state.people);
+    let people = this.state.people.slice();
+    // console.log(people);
+    people = people.filter(person => name !== person.name) // przekaż wszystkie inne niż te które mają true
+    console.log(people);
     this.setState({
       people
     })
   }
 
   render() {
+
+    const people = this.state.people.map(person =>
+      <Person
+        key={person.id}
+        name={person.name}
+        delete={this.handelDelete.bind(this, person.name)} />)
+
     return (
-      <ul>{this.state.people.map(person => <Person key={person.id} name={person.name} delete={this.handelDelete.bind(this, person.id)} />)}</ul>
+      <ul>{people}</ul>
     )
   }
 }
